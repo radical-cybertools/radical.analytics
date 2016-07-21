9845e2d6e9b0fba999c7322f04334a75bb1f9d63
mturilli <matteo.turilli@gmail.com>
2016-07-21 08:01:11 +0200

    Iteration documentation

--- examples/04_session_duration_rp.py
+++ examples/04_session_duration_rp.py
@@ -46,11 +46,10 @@ if __name__ == '__main__':
         print separator + message + separator
 
     # and here we go. Once we filter our session object so to keep only the
-    # entities that are relevant to our analysis (as seen in example 03), we are
-    # ready to perform that analysis :) Currently, RADICAL-Analytics supports
-    # two types of analysis: duration and concurrency. This examples shows how
-    # to use the RA API to performan duration analysis using both states and
-    # events.
+    # relevent entities (as seen in example 03), we are ready to perform our
+    # analyses :) Currently, RADICAL-Analytics supports two types of analysis:
+    # duration and concurrency. This examples shows how to use the RA API to
+    # performan duration analysis, using both states and events.
     #
     # First we look at the state model of our session. We saw how to print it
     # out in example 00:
@@ -61,45 +60,43 @@ if __name__ == '__main__':
     # been active. Looking at the state model of the entity of type 'pilot' and
     # to the documentation of RADICAL-Pilot, we know that a pilot is active
     # between the state 'ACTIVE' and one of the three final states 'DONE',
-    # 'CANCELED', 'FAILED'.
+    # 'CANCELED', 'FAILED' of all the entities of RP.
     ppheader("Time spent by the pilots being active")
     pilots = session.filter(etype='pilot', inplace=False)
     durations = pilots.duration([rp.ACTIVE, [rp.DONE, rp.CANCELED, rp.FAILED]])
     pprint.pprint(durations)
 
-    # And now we want to do the same for the all the entities of type 'unit':
+    # Now, we want to do the same for the all the entities of type 'unit':
     ppheader("Time spent by the units being active")
     units = session.filter(etype='unit', inplace=False)
     duration_active = units.duration([rp.AGENT_EXECUTING, [rp.DONE, rp.CANCELED, rp.FAILED]])
     pprint.pprint(duration_active)
 
     # The careful reader will have noticed that the previous duration includes
-    # both the time spent by the units to execute and to stage data out. We can
-    # separate the two by using instead of the final states, the state following
-    # immediately after 'AGENT_EXECUTING', i.e., 'AGENT_STAGING_OUTPUT_PENDING':
+    # the time spent by the units to execute and to stage data out. We can
+    # separate the two by using the state 'AGENT_STAGING_OUTPUT_PENDING',
+    # instead of the final states:
     ppheader("Time spent by the units executing their kernel")
     units = session.filter(etype='unit', inplace=False)
     duration_exec = units.duration([rp.AGENT_EXECUTING, rp.AGENT_STAGING_OUTPUT_PENDING])
     pprint.pprint(duration_exec)
 
-    # and calculating the time spent doing staging out by all entities of type
-    # 'unit':
+    # We calculate the time spent doing staging out by all entities of type
+    # 'unit' with 'AGENT_STAGING_OUTPUT_PENDING' and the final states:
     ppheader("Time spent by the units performing staging out")
     units = session.filter(etype='unit', state=rp.DONE, inplace=False)
     duration_sout = units.duration([rp.AGENT_STAGING_OUTPUT_PENDING, [rp.DONE, rp.CANCELED, rp.FAILED]])
     pprint.pprint(duration_sout)
 
-    print """
-    The very careful reader may have noticed that the sum of the time spent by
-    the units to execute their kernel and performing staging out may be
-    greater than the time spent by the units being active. This is explained
-    by the potential overlapping of the time spent executing and staging out.
-    This overlapping is accounted for when calculating the time spent by the
-    units being active."""
+    print """ The very careful reader may have noticed that the sum of the time
+    spent by the units to execute their kernel and performing staging out may be
+    greater than the time spent by the units being active. This is explained by
+    the potential overlapping between the time spent executing the kernell and
+    that spent staging out. This overlapping is accounted for when calculating
+    the time spent by the units being active."""
 
     ppheader("Ovelapping between executing and staging")
     overlap = (duration_exec + duration_sout) - duration_active
     print overlap
-
 
     sys.exit(0)
