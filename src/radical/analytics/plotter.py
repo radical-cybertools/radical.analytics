90c28ac97b1e2412a14d17d63a5d467b222875a5
Ioannis Paraskevakos <i.paraskev@rutgers.edu>
2017-09-14 19:58:27 -0400

    Comments, comments

--- src/radical/analytics/plotter.py
+++ src/radical/analytics/plotter.py
@@ -22,16 +22,39 @@ class Plotter(object):
         self._plot_grid = plot_grid
         
     def utilization(self,util_data=None,normalized=None,resource=None,fig_size=None):
+
+        """
+        This method receives as input utilization data of a specific resource and creates 
+        a figure for that specific utilization.
+
+        The method is able to normalized the plot based on the owner resources and the
+        utilized resources at any given time. In addition, figure's size can be provided as
+        input
+        """
         
+        # Initially check if plot_grid is set. If not, create a single plot, else create
+        # a figure based on the user's specification.
         if not self._plot_grid:
+
             self._fig,self._axis = plt.subplots(nrows=1,ncols=1,figsize=fig_size)
+
+            # Iterate over the owners and add a line for every owner.
             for key, util in util_data.iteritems():
+
+                # Getting the time moments where the utilization changes
                 x_axis = [point[0] for point in util['utilization']]
+
+                # Getting the utilization points. If normalized flag is set, utilization is
+                # divided with the total resources of the owner and multiply with 100
                 if normalized:
                     y_axis = [(point[1]/util['resources'])*100 for point in util['utilization']]
                 else:
                     y_axis = [point[1] for point in util['utilization']]
-                resource_range = util['range'] if util['range'] else 0
+
+                # If there is a range where the owner had the resources get it. Otherwise
+                # it is none. The resource range will be use to set the utilization plot
+                # correctly in the x-axis
+                resource_range = util['range'] if util['range'] else None
                 
                 if resource_range:
                     self._axis.set_xlim(resource_range)
@@ -39,6 +62,7 @@ class Plotter(object):
                 else:
                     self._axis.plot(x_axis,y_axis,label='%s utilization'%key)
                 
+                # X,Y label, and title are being set
                 self._axis.set_ylabel('Utilization in Resources')
                 self._axis.set_xlabel('Time in seconds')
                 self._axis.set_title('%s Resource Utilization'%resource)
@@ -47,11 +71,20 @@ class Plotter(object):
         else:
             self._fig,self._axis = plt.subplots(nrows=self._plot_grid[0],ncols=self._plot_grid[1],figsize=fig_size)
             for (key, util),i in zip(util_data.iteritems(),range(self._plot_grid[0]*self._plot_grid[1])):
+                
+                # Getting the time moments where the utilization changes
                 x_axis = [point[0] for point in util['utilization']]
+                
+                # Getting the utilization points. If normalized flag is set, utilization is
+                # divided with the total resources of the owner and multiply with 100
                 if normalized:
                     y_axis = [(point[1]/util['resources'])*100 for point in util['utilization']]
                 else:
                     y_axis = [point[1] for point in util['utilization']]
+                
+                # If there is a range where the owner had the resources get it. Otherwise
+                # it is none. The resource range will be use to set the utilization plot
+                # correctly in the x-axis
                 resource_range = util['range'] if util['range'] else 0
                 
                 if resource_range:
@@ -60,6 +93,7 @@ class Plotter(object):
                 else:
                     self._axis[i].plot(x_axis,y_axis,label='%s utilization'%key)
             
+                # X,Y label, and title are being set
                 self._axis[i].set_ylabel('Utilization in Resources')
                 self._axis[i].set_xlabel('Time in seconds')
                 self._axis[i].set_title('%s Resource Utilization'%resource)
@@ -67,9 +101,22 @@ class Plotter(object):
 
     def concurrency(self,data=None,fig_size=None):
         
+        """
+        This method receives as input concurrency data and creates a figure.
+
+        The user is able to set the figure size.
+        """
+
+
         self._fig,self._axis = plt.subplots(nrows=1,ncols=1,figsize=fig_size)
+
+        # The x axis is setup.
         x_axis = [point[0] for point in data]
+
+        # The y axis is setup.
         y_axis = [point[1] for point in data]
+
+        #Just plot
         self._axis.plot(x_axis,y_axis)        
         self._axis.set_ylabel('Concurrent Entities')
         self._axis.set_xlabel('Time in seconds')
