b2e5e38015c04a950dfa1cdcd12ca1a19a861890
Andre Merzky <andre@merzky.net>
2019-02-02 14:24:10 +0100

    introduce a native radical.analytics session (#63)
    * introduce a native radical.analytics session
    
    which consumes profiles w/o session description etc.
    
    * reset example
    
    * add RA example
    
    * support radical.stats
    add a simple session stub for git issue analysis (radical.stats) via analytics
    
    * added license file
    
    * version bump
    
    * fix license inconsistency
    
    * version bump
    
    * make pypi happy
    
    * version bump
    
    * small parsing fixes

--- src/radical/analytics/session.py
+++ src/radical/analytics/session.py
@@ -49,11 +49,14 @@ class Session(object):
                  src.endswith('.tar.bz')    :
                 tgt = src[:-7]
 
+            elif src.endswith('.prof'):
+                tgt = None
+
             else:
                 raise ValueError('src does not look like a tarball or profile')
 
 
-            if not os.path.exists(tgt):
+            if tgt and not os.path.exists(tgt):
 
                 # need to extract
                 print 'extract tarball to %s' % tgt
@@ -65,7 +68,8 @@ class Session(object):
                     raise RuntimeError('Cannot extract tarball: %s' % repr(e))
 
             # switch to the extracted data dir
-            src = tgt
+            if tgt:
+                src = tgt
 
 
         # if no sid is given, we assume its the directory name
@@ -81,39 +85,51 @@ class Session(object):
       # print 'sid: %s [%s]' % (sid, stype)
       # print 'src: %s'      % src
 
-        if stype == 'radical.pilot':
-            import radical.pilot as rp
+        if stype == 'radical':
+
+            # src is expected to point either to a single profile, or to
+            # a directory tree containing profiles
+            if not src:
+                raise ValueError('RA session types need `src` specified')
+
+            profiles = list()
+            if os.path.isfile(src):
+                profiles.append(src)
+            else:
+                for root, dirs, files in os.walk(src):
+                    for f in files:
+                        if f.endswith('.prof'):
+                            profiles.append('%s/%s' % (root, f))
+
+            profiles                = ru.read_profiles(profiles, sid=sid)
+            self._profile, accuracy = ru.combine_profiles(profiles)
+            self._description       = {'tree'     : dict(),
+                                       'entities' : list(),
+                                       'hostmap'  : dict(),
+                                       'accuracy' : 0.0}
+
+        elif stype == 'radical.pilot':
+
+            import radical.pilot.utils as rpu
             self._profile, accuracy, hostmap \
-                              = rp.utils.get_session_profile(sid=sid, src=self._src)
-            self._description = rp.utils.get_session_description(sid=sid, src=self._src)
+                              = rpu.get_session_profile    (sid=sid, src=self._src)
+            self._description = rpu.get_session_description(sid=sid, src=self._src)
 
             self._description['accuracy'] = accuracy
             self._description['hostmap']  = hostmap
 
 
         elif stype == 'radical.entk':
-            import radical.entk as re
 
-            self._profile, accuracy, hostmap = re.utils.get_session_profile(sid=sid, src=self._src)
-            self._description = re.utils.get_session_description(sid=sid, src=self._src)
+            import radical.entk.utils as reu
+
+            self._profile, accuracy, hostmap \
+                              = reu.get_session_profile    (sid=sid, src=self._src)
+            self._description = reu.get_session_description(sid=sid, src=self._src)
 
             self._description['accuracy'] = accuracy
             self._description['hostmap']  = hostmap
 
-
-        elif stype == 'radical.prof':
-
-            if os.path.isdir(src): profiles = glob.glob("%s/*.prof")
-            else                 : profiles = [src]
-
-            profiles          = ru.read_profiles(profiles, src)
-            profile, accuracy = ru.combine_profiles(profiles)
-            self._profile     = ru.clean_profile(profile, src)
-
-            self._description = {'tree'     : dict(), 
-                                 'entities' : list()}
-            self._description['accuracy'] = accuracy
-            self._description['hostmap']  = dict()
 
         else:
             raise ValueError('unsupported session type [%s]' % stype)
@@ -232,8 +248,8 @@ class Session(object):
         # all events for that uid)
         for uid,events in entity_events.iteritems():
             etype   = events[0][ru.ENTITY]
-            details = self._description['tree'].get(uid, dict())
-            details['hostid'] = self._description['hostmap'].get(uid)
+            details = self._description.get('tree', dict()).get(uid, dict())
+            details['hostid'] = self._description.get('hostmap', dict()).get(uid)
             self._entities[uid] = Entity(_uid=uid,
                                          _etype=etype,
                                          _profile=events,
@@ -470,7 +486,7 @@ class Session(object):
             state_values = None
             event_model  = None
 
-            if et in self._description['entities']:
+            if et in self._description.get('entities', dict()):
                 state_model  = self._description['entities'][et]['state_model']
                 state_values = self._description['entities'][et]['state_values']
                 event_model  = self._description['entities'][et]['event_model']
@@ -505,7 +521,7 @@ class Session(object):
             parent_uids = self._apply_filter(etype=etype[0])
             child_uids  = self._apply_filter(etype=etype[1])
 
-            rel = self._description['tree']
+            rel = self._description.get('tree', dict())
             for p in parent_uids:
 
                 ret[p] = list()
