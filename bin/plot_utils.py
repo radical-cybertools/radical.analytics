97052685d78a7a3696d5c1d908a581f699cd41aa
mturilli <matteo.turilli@gmail.com>
2021-06-25 18:49:19 +0200

    Mpl style and plot size function

---
+++ bin/plot_utils.py
@@ -0,0 +1,30 @@
+def set_size(width, fraction=1, subplots=(1, 1)):
+    """ Set aesthetic figure dimensions to avoid scaling in latex.
+
+    Parameters
+    ----------
+    width   : float
+              Width in pts
+    fraction: float
+              Fraction of the width which you wish the figure to occupy
+
+    Returns
+    -------
+    fig_dim : tuple
+              Dimensions of figure in inches
+    """
+    # Width of figure
+    fig_width_pt = width * fraction
+
+    # Convert from pt to inches
+    inches_per_pt = 1 / 72.27
+
+    # Golden ratio to set aesthetic figure height
+    golden_ratio = (5 ** 0.5 - 1) / 2
+
+    # Figure width in inches
+    fig_width_in = fig_width_pt * inches_per_pt
+    # Figure height in inches
+    fig_height_in = fig_width_in * golden_ratio * (subplots[0] / subplots[1])
+
+    return fig_width_in, fig_height_in
