20a9122015f3a3bb7d8e8c483bf8dabee4d3a32f
Matteo Turilli <matteo.turilli@gmail.com>
2021-07-06 14:51:24 -0400

    Address comment

--- src/radical/analytics/utils/__init__.py
+++ src/radical/analytics/utils/__init__.py
@@ -1,6 +1,7 @@
 # ------------------------------------------------------------------------------
 #
-from .plot import get_plotsize, get_mplstyle, stack_transitions, get_pilot_series, get_plot_utilization, get_pilots_zeros
+from .plot import get_plotsize, get_mplstyle, stack_transitions, \
+           get_pilot_series, get_plot_utilization, get_pilots_zeros
 
 # ------------------------------------------------------------------------------
 #
