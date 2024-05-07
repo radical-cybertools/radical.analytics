b08059393b1bfebe71394f1698f77741c391920e
Andre Merzky <andre@merzky.net>
2024-05-08 01:33:11 +0200

    sync with RU

--- src/radical/analytics/__init__.py
+++ src/radical/analytics/__init__.py
@@ -20,25 +20,15 @@ from .utils import tabulate_durations
 
 # ------------------------------------------------------------------------------
 #
-import os
-import radical.utils as ru
+import os as _os
+import radical.utils as _ru
 
-pwd  = os.path.dirname (__file__)
-root = "%s" % pwd
-version_short, version_detail, version_base, \
-        version_branch, sdist_name, sdist_path = ru.get_version(paths=[root])
-version = version_short
+_mod_root = _os.path.dirname (__file__)
 
-logger = ru.Logger('radical.analytics', targets=['-'])
-logger.info('radical.analytics    version: %s' % version_detail)
-
-
-# ------------------------------------------------------------------------------
-#
-__all__ = ('Experiment','Session','Entity','Plotter', 'get_plotsize',
-           'get_mplstyle', 'stack_transitions', 'get_pilot_series',
-           'get_plot_utilization', 'get_pilots_zeros', 'to_latex',
-           'tabulate_durations')
+version_short, version_base, version_branch, version_tag, version_detail \
+             = _ru.get_version(_mod_root)
+version      = version_short
+__version__  = version_detail
 
 
 # ------------------------------------------------------------------------------
