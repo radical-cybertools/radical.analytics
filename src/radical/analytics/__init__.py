79d5e1dc55c7d1daffaaa993a434a6368f40a97d
Andre Merzky <andre@merzky.net>
2024-05-08 01:45:48 +0200

    sync with RU

--- src/radical/analytics/__init__.py
+++ src/radical/analytics/__init__.py
@@ -1,6 +1,15 @@
 
-__copyright__ = "Copyright 2013-2020, http://radical.rutgers.edu"
+__copyright__ = "Copyright 2013-2024, http://radical.rutgers.edu"
 __license__   = "MIT"
+
+
+# ------------------------------------------------------------------------------
+# we *first* import radical.utils, so that the monkeypatching of the logger has
+# a chance to kick in before the logging module is pulled by any other 3rd party
+# module, and also to monkeypatch `os.fork()` for the `atfork` functionality
+#
+import os            as _os
+import radical.utils as _ru
 
 
 # ------------------------------------------------------------------------------
@@ -10,8 +19,7 @@ from .session    import Session
 from .entity     import Entity
 from .plotter    import Plotter
 
-# ------------------------------------------------------------------------------
-#
+
 from .utils import get_plotsize, get_mplstyle, stack_transitions
 from .utils import get_pilot_series, get_plot_utilization, get_pilots_zeros
 from .utils import to_latex
@@ -20,9 +28,8 @@ from .utils import tabulate_durations
 
 # ------------------------------------------------------------------------------
 #
-import os as _os
-import radical.utils as _ru
-
+# get version info
+#
 _mod_root = _os.path.dirname (__file__)
 
 version_short, version_base, version_branch, version_tag, version_detail \
