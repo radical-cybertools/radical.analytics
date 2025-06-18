596efbec87d57319e114c4f8bcda7d77ea8a7483
Andre Merzky <andre@merzky.net>
2025-06-18 10:49:42 +0200

    snap

--- bin/rp_inspect/plot_state.py
+++ bin/rp_inspect/plot_state.py
@@ -25,33 +25,27 @@ plt.style.use(ra.get_mplstyle("radical_mpl"))
 # type `event_entity`..  Before plotting, we sort those entities by the
 # timestamp of the first event in the event list
 
+s = ru.STATE
+e = ru.EVENT
 event_entities = ['task', 'master', 'worker']
 event_list   = [
-    # {ru.STATE: rp.NEW                         , ru.EVENT: 'state'          },
-    # {ru.STATE: rp.TMGR_SCHEDULING_PENDING     , ru.EVENT: 'state'          },
-    # {ru.STATE: rp.TMGR_SCHEDULING             , ru.EVENT: 'state'          },
-    # {ru.STATE: rp.TMGR_STAGING_INPUT_PENDING  , ru.EVENT: 'state'          },
-    # {ru.STATE: rp.TMGR_STAGING_INPUT          , ru.EVENT: 'state'          },
-    # {ru.STATE: rp.AGENT_STAGING_INPUT_PENDING , ru.EVENT: 'state'          },
-    # {ru.COMP : 'agent_0'                      , ru.EVENT: 'get'            },
-      {ru.STATE: rp.AGENT_STAGING_INPUT         , ru.EVENT: 'state'          },
-      {ru.STATE: rp.AGENT_SCHEDULING_PENDING    , ru.EVENT: 'state'          },
-      {ru.STATE: rp.AGENT_SCHEDULING            , ru.EVENT: 'state'          },
-    # {ru.STATE: None                           , ru.EVENT: 'schedule_ok'    },
-      {ru.STATE: rp.AGENT_EXECUTING_PENDING     , ru.EVENT: 'state'          },
-      {ru.STATE: rp.AGENT_EXECUTING             , ru.EVENT: 'state'          },
-    # {ru.STATE: None                           , ru.EVENT: 'exec_mkdir'     },
-    # {ru.STATE: None                           , ru.EVENT: 'exec_mkdir_done'},
-      {ru.STATE: None                           , ru.EVENT: 'exec_start'     },
-    # {ru.STATE: None                           , ru.EVENT: 'app_start'      },
-    # {ru.STATE: None                           , ru.EVENT: 'app_stop'       },
-    # {ru.STATE: None                           , ru.EVENT: 'exec_ok'        },
-      {ru.STATE: None                           , ru.EVENT: 'exec_stop'      },
-      {ru.STATE: rp.AGENT_STAGING_OUTPUT_PENDING, ru.EVENT: 'state'          },
-      {ru.STATE: rp.AGENT_STAGING_OUTPUT        , ru.EVENT: 'state'          },
-    # {ru.STATE: rp.TMGR_STAGING_OUTPUT_PENDING , ru.EVENT: 'state'          },
-    # {ru.STATE: rp.TMGR_STAGING_OUTPUT         , ru.EVENT: 'state'          },
-    # {ru.STATE: rp.DONE                        , ru.EVENT: 'state'          },
+    # [{s: rp.AGENT_STAGING_INPUT_PENDING , e: 'state'          }, 'input_wait' ],
+    # [{s: rp.AGENT_STAGING_INPUT         , e: 'state'          }, 'input'      ],
+    # [{s: rp.AGENT_SCHEDULING_PENDING    , e: 'state'          }, 'sched_wait' ],
+      [{s: rp.AGENT_SCHEDULING            , e: 'state'          }, 'sched'      ],
+    # [{s: None                           , e: 'schedule_ok'    }, 'sched_ok'   ],
+      [{s: rp.AGENT_EXECUTING_PENDING     , e: 'state'          }, 'exec_wait'  ],
+      [{s: rp.AGENT_EXECUTING             , e: 'state'          }, 'exec'       ],
+    # [{s: None                           , e: 'exec_mkdir'     }, 'mkdir'      ],
+    # [{s: None                           , e: 'exec_mkdir_done'}, 'mkdir_ok'   ],
+      [{s: None                           , e: 'exec_start'     }, 'exec_start' ],
+      [{s: None                           , e: 'exec_stop'      }, 'exec_stop'  ],
+    # [{s: None                           , e: 'app_start'      }, 'app_start'  ],
+    # [{s: None                           , e: 'app_stop'       }, 'app_stop'   ],
+    # [{s: None                           , e: 'exec_ok'        }, 'exec_ok'    ],
+      [{s: None                           , e: 'exec_stop'      }, 'exec_stop'  ],
+    # [{s: rp.AGENT_STAGING_OUTPUT_PENDING, e: 'state'          }, 'output_wait'],
+    # [{s: rp.AGENT_STAGING_OUTPUT        , e: 'state'          }, 'output'     ],
 ]
 
 
@@ -74,7 +68,8 @@ if __name__ == '__main__':
 
         tstamps = list()
 
-        for event in event_list:
+        for item in event_list:
+            event = item[0]
             times = thing.timestamps(event=event)
             if times: tstamps.append(times[0])
             else    : tstamps.append(None)
@@ -113,17 +108,19 @@ if __name__ == '__main__':
 
     np_data = np.array(sorted_data)
 
-    fig, ax = plt.subplots(figsize=ra.get_plotsize(500))
+    fig, ax = plt.subplots(figsize=ra.get_plotsize(252))
 
-    for e_idx in range(len(event_list)):
-        ax.plot(np_data[:,0], np_data[:,(1 + e_idx)],
-                 label=to_latex(ru.event_to_label(event_list[e_idx])))
+    for idx,item in enumerate(event_list):
+        print(idx, item[1], item[0])
+        ax.plot(np_data[:,0], np_data[:,(1 + idx)],
+                 label=to_latex(item[1]))
 
     plt.xlabel(to_latex('task (sorted by %s)' % order))
     plt.ylabel(to_latex('time [sec]'))
 
     # FIXME: how to do the legend now?  With the large font size, I don't see
-    plt.legend()
+    plt.legend(loc='upper center', bbox_to_anchor=(0.5, 1.20),
+          ncol=3, fancybox=True, shadow=True)
     plt.savefig('%s.state.png' % session.uid)
   # plt.show()
 
